f1da42541d02fcea79fe5d9071d157401f549505
stephym <60659457+stephymelo@users.noreply.github.com>
2020-04-24 23:35:16 -0500

    somethings

--- Main.js
+++ Main.js
@@ -5,9 +5,14 @@ let pantalla;
 
 function setup(){
     createCanvas(500,500);
-    cuadrados = new Cuadrado();
+
+    arrayCuadrados=[];
+    arrayCirculos=[];
+
+    
     clicky=false;
     pantalla=0;
+    nextPantalla=false;
     numero=0;
     
 
@@ -31,25 +36,29 @@ function draw(){
 function buttons(){
     switch(pantalla){
         case 0 :
-            fill(255);
-            textSize(100);
-            text(numero,220,100);
+           
+            
             stroke(255);
             fill(235, 205, 167);
+            textSize(100);
+            text(numero,220,150);
             rect(50,300,150,50,20);  
-            rect(300,300,150,50,20);  
-            if(numero>=1&&numero!=0){
-                rect(170,400,150,50,20);  
-            }
+            rect(300,300,150,50,20); 
+            rect(170,400,150,50,20);   
+            // if(numero>=1&&numero!=0){
+            //     rect(170,400,150,50,20);  
+            //     nextPantalla=true;
+            // }
             fill(250);
             textSize(20);
             text("Aumentar Num",55,330);
             text("Decrecer Num",305,330);
-            if(numero>=1&&numero!=0){
             text("Crear Figuritas",175,430);
-            }
+         
             if(clicky==true){
                 text("mensaje",50,50);
+            }else{
+
             }
            
            
@@ -71,6 +80,7 @@ function buttons(){
 
 function mouseClicked(){
     if(mouseX>50&&mouseY>300&&mouseX<150&&mouseY<350){
+   
         if(numero<10){
             numero++;  
         }else{
@@ -86,7 +96,6 @@ function mouseClicked(){
             
         }
    
-     
     }
     
     try {
@@ -97,12 +106,53 @@ function mouseClicked(){
         console.error(error);
         
       }
+
+if(mouseX>170&&mouseY>400&&mouseX<230&&mouseY<450){
+    console.log("alo");
+
+    try {
+        validarNum2();
+      }
+      catch(error) {
+      
+        console.error(error);
+        
+      }
+    //   finally{
+    //     pantalla=1;
+
+    //   }
+    pantalla=1;
+    cuadrado.push(new Cuadrado(10,200,50,50));
+
+
 }
+}
+
+
+function drawFiguras(){
+    for(let i=0;i<arrayCuadrados.length;i++){
+        e=arrayCuadrados[i];
+
+    }
+}
+
+
+
+
+
 
 function validarNum(){
     if(numero==10){
-        throw new Exception("llegasta a diez, no puedes mas bb");
+        throw new Exception("llegasta a diez, no puedes mas");
     }
     
     
+}
+
+function validarNum2(){
+
+    if(numero==0){
+        throw new Exception("cant pass");        
+    }
 }
